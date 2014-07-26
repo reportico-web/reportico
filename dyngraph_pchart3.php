ef3f293a8425b73340ebc0fba5497366256ca3cb
Peter Deed <peterdeed@reportico.org>
2014-07-26 11:06:44 +0100

    Fix comment

--- dyngraph_pchart3.php
+++ dyngraph_pchart3.php
@@ -17,7 +17,7 @@
  along with this program; if not, write to the Free Software
  Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
 
- * File:        reportico.php
+ * File:        dyngraph_pchart3.php
  *
  * Generates a chart image from axes, data parameters
  *
