0dcafea8f0a201e6e9fe335228c733789b0bfb25
Peter Deed <info@reportico.org>
2014-12-06 15:57:27 +0000

    Addded example customie function

--- run.php
+++ run.php
@@ -288,8 +288,8 @@
     */
     function reportico_customize($reportico)
     {
-            //$reportico->create_page_header("pp", 1, "188888{STYLE width:200;height:1cm;margin: 6cm 0cm 0cm 5cm; color: #ffee00; background-color:#000000}" );
-            //$reportico->create_page_header("qq", 1, "200000 skdfjsl dfjlskj dfksjdf sldfj lkj{STYLE width:200;height:1cm;background-color:#00aa22; margin: 4cm 0cm 0cm 1cm; }" );
-            //$reportico->create_page_header("rr", 1, "399999{STYLE width:200;height:1cm;margin: 2cm 0cm 0cm 8cm; background-color:#00aaee}" );
+            //$reportico->create_page_header("H1", 1, "Header1{STYLE width:200;height:1cm;margin: 6cm 0cm 0cm 5cm; color: #ffee00; background-color:#000000}" );
+            //$reportico->create_page_header("H2", 1, "Header2 skdfjsl dfjlskj dfksjdf sldfj lkj{STYLE width:200;height:1cm;background-color:#00aa22; margin: 4cm 0cm 0cm 1cm; }" );
+            //$reportico->create_page_header("H3", 1, "Header3{STYLE width:200;height:1cm;margin: 2cm 0cm 0cm 8cm; background-color:#00aaee}" );
     }   
 ?>
