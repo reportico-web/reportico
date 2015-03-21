a320c0f9c1a4d2ed55acbd7b4621d4e45018d937
Peter Deed <info@reportico.org>
2015-03-21 15:36:59 +0000

    Commented out vardump

--- swutil.php
+++ swutil.php
@@ -699,9 +699,9 @@ function ErrorHandler($errno, $errstr, $errfile, $errline)
 		"errsource" => $g_code_source,
 		"errct" => 1
 		);
-	echo "<PRE>";
-	var_dump($g_system_errors);
-	echo "</PRE>";
+	//echo "<PRE>";
+	//var_dump($g_system_errors);
+	//echo "</PRE>";
 
     $g_error_status = 1;
 
