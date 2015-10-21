0e40c86832a6c951f70e53fed805aec13b755613
nmcgann <neil@neilmcgann.co.uk>
2015-10-21 07:29:26 +0100

    js fix

--- js/reportico.js
+++ js/reportico.js
@@ -466,13 +466,13 @@ reportico_jquery.fn.serializeObject = function() {
   reportico_jquery.each(arrayData, function() {
     var value;
 
-    if (this.value !== null) {
+    if (this.value != null) {
       value = this.value;
     } else {
       value = '';
     }
 
-    if (objectData[this.name] !== null) {
+    if (objectData[this.name] != null) {
       if (!objectData[this.name].push) {
         objectData[this.name] = [objectData[this.name]];
       }
