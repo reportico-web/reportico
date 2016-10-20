d346b13a06a47337b1a1278eb17a90c4e72ff102
Peter Deed <info@reportico.org>
2016-10-20 19:56:08 +0100

    Added Brazilian Portuguese

---
+++ language/packs/pt_br/menu.php
@@ -0,0 +1,19 @@
+<?php
+$locale_arr = array (
+    "language" => "Português Brasileiro",
+    "template" => array (
+        "T_CHOOSE_LANGUAGE" => "Escolha o idioma",
+        "T_GO" => "Ir",
+        "T_LOGGED_IN_AS" => "Logado como",
+        "T_LOGIN" => "Entrar",
+        "T_LOGOFF" => "Sair",
+        "T_ADMIN_HOME" => "Pagina de Administracao",
+        "T_CONFIG_PROJECT" => "Configurar Projeto",
+        "T_CREATE_REPORT" => "Criar Relatorio",
+        "T_ENTER_PROJECT_PASSWORD" => "Informe a senha do projeto.",
+        "T_ENTER_PROJECT_PASSWORD_DEMO" => "Informe a senha do projeto. <br>A senha para estes tutoriais e <b>reportico</b>",
+        "T_UNABLE_TO_CONTINUE" => "Não foi possivel continuar",
+        "T_PASSWORD_ERROR" => "Senha incorreta. Tente novamente.",
+        ),
+        );
+?>
