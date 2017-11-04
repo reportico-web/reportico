6676f7f9add2bfbd713aa6fa04c9b8e4cf202df2
Peter Deed <peterdeed@nimbusjourneyinfo.co.uk>
2017-11-04 21:13:48 +0000

    Minor chages to to tidyup run.php and gitignore

--- run.php
+++ run.php
@@ -271,12 +271,6 @@
     // How CSV, PDF out is delivered to the browser ( TCPDF output only )
     // either as
     // "DOWNLOAD_SAME_WINDOW" - downloaded as attachment from within the current browser window ( default )
-    // "INLINE" - shown inside a new  browser window making use of any existing browser PDF plugin (if not will download)
-    // "DOWNLOAD_NEW_WINDOW" - downloaded as attachment from winthin the current browser window
-    ////$q->pdf_delivery_mode = "INLINE";
-    ////$q->pdf_delivery_mode = "DOWNLOAD_NEW_WINDOW";
-    $q->pdf_delivery_mode = "INLINE";
-    //$q->pdf_delivery_mode = "DOWNLOAD_NEW_WINDOW";
     //$q->pdf_delivery_mode = "DOWNLOAD_SAME_WINDOW";
 
     // Dropdown Menu definition
