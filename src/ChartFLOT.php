35ded306aaf5cbbd8d5e2da07a66ec0e24e8401b
Peter Deed <peter.deed@r2p.com>
2018-04-29 17:30:39 +0100

    Changed to MIT license

--- src/ChartFLOT.php
+++ src/ChartFLOT.php
@@ -1,21 +1,5 @@
 <?php
 /*
- Reportico - PHP Reporting Tool
- Copyright (C) 2010-2014 Peter Deed
-
- This program is free software; you can redistribute it and/or
- modify it under the terms of the GNU General Public License
- as published by the Free Software Foundation; either version 2
- of the License, or (at your option) any later version.
- 
- This program is distributed in the hope that it will be useful,
- but WITHOUT ANY WARRANTY; without even the implied warranty of
- MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- GNU General Public License for more details.
-
- You should have received a copy of the GNU General Public License
- along with this program; if not, write to the Free Software
- Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
 
  * File:        swgraph_flot.php
  *
@@ -29,7 +13,6 @@
  * @copyright 2010-2014 Peter Deed
  * @author Peter Deed <info@reportico.org>
  * @package Reportico
- * @license - http://www.gnu.org/licenses/gpl-2.0.html GNU/GPL
  * @version $Id: swgraph_flot.php,v 1.15 2013/09/26 19:23:06 peter Exp $
  */
 
--- src/ReportSoapTemplate.php
+++ src/ReportSoapTemplate.php
@@ -1,21 +1,5 @@
 <?php
 /*
-Reportico - PHP Reporting Tool
-Copyright (C) 2010-2014 Peter Deed
-
-This program is free software; you can redistribute it and/or
-modify it under the terms of the GNU General Public License
-as published by the Free Software Foundation; either version 2
-of the License, or (at your option) any later version.
-
-This program is distributed in the hope that it will be useful,
-but WITHOUT ANY WARRANTY; without even the implied warranty of
-MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-GNU General Public License for more details.
-
-You should have received a copy of the GNU General Public License
-along with this program; if not, write to the Free Software
-Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
 
  * File:        ReportSoapTemplate.php
  *
@@ -28,7 +12,6 @@ Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
  * @copyright 2010-2014 Peter Deed
  * @author Peter Deed <info@reportico.org>
  * @package Reportico
- * @license - http://www.gnu.org/licenses/gpl-2.0.html GNU/GPL
  * @version $Id: swoutput.php,v 1.33 2014/05/17 15:12:31 peter Exp $
  */
 namespace Reportico\Engine;
