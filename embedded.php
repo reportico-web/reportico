8b9640305e97b0e9ae61e6a2dc3cc0b1d8f8d02c
Peter Deed <peter.deed@r2p.com>
2018-08-03 09:19:23 +0100

    Embedded example to work with ^6.0.0

--- embedded.php
+++ embedded.php
@@ -1,19 +1,23 @@
 <?php
 	ob_start();
 ?>
-<H1>EMBEDDED REPORT DEMONSTRATION</H1>
-<H1>START OF WEB PAGE TEXT FOLLOWED BY REPORTICO OUTPUT</H1>
+<H1>START OF WEB PAGE TEXT BEFORE REPORTICO OUTPUT</H1>
+
 <?php
-    date_default_timezone_set(@date_default_timezone_get());
-
-	error_reporting(E_ALL);
-
-	require_once('reportico.php');
-	$a = new Reportico();
-	$a->allow_maintain = "FULL";
-	$a->allow_debug = true;
-	$a->embedded_report = true;
-	$a->execute();
+require_once('vendor/autoload.php');
+$q = new Reportico\Engine\Reportico();
+$q->pdf_delivery_mode = "DOWNLOAD_SAME_WINDOW";
+$q->setTheme('default');
+$q->access_mode = "ONEPROJECT";
+$q->initial_execute_mode = "PREPARE";
+$q->initial_project = "tutorials";
+$q->initial_report = "stock";
+$q->bootstrap_styles = "3";
+$q->force_reportico_mini_maintains = false;
+$q->bootstrap_preloaded = false;
+$q->clear_reportico_session = true;
+$q->reportico_ajax_mode = true;
+$q->execute();
 ?>
 
 <H1>END OF WEB PAGE TEXT FOLLOWING REPORTICO OUTPUT</H1>
