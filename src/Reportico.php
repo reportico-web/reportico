d06c11df1fcbbf2119be077c799b30ec7737883d
Peter Deed <info@reportico.org>
2018-11-10 14:08:13 +0000

    Version 6.0.9

--- src/Reportico.php
+++ src/Reportico.php
@@ -49,7 +49,7 @@ class Reportico extends ReporticoObject
     public $delete_project_url;
     public $create_report_url;
 
-    public $version = "6.0.7";
+    public $version = "6.0.9";
     public $doc_version = "6.0.0";
 
     public $name;
